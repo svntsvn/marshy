11bc7ea02f10a98a32130d35ff3868c62af7dee0
svntsvn <gary.mathos@gmail.com>
2020-09-25 17:32:30 -0400

    all files

---
+++ layouts/default.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<style>
+
+html {
+    font-family: 'Source Sans Pro', sans-serif;
+    font-size: 16px;
+    word-spacing: 1px;
+    -ms-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    box-sizing: border-box;
+  }
+
+  p {
+    font-family: 'Source Sans Pro', sans-serif;
+
+  }
+
+.text-gold {
+    color: rgba(141, 121, 65, 1);
+}
+
+
+
+
+h1 {
+    font-family: 'Literata', serif;
+  
+  
+  
+  }
+
+ 
+.page-enter-active,
+.page-leave-active {
+  transition: opacity .5s;
+  
+
+}
+.page-enter,
+.page-leave-to {
+  opacity: 0;
+  
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex-column content-center justify-center align-middle">
+    <video-background class="z-0 fixed"
+    src="desktop.mp4"
+    poster="desktop.png"
+    :sources="[
+        {src: 'desktop.mp4', res: 900, autoplay: true,  poster: 'mobile_vert_poster_1.png'}, 
+        {src: 'mobile_vert_1.mp4', res: 638, autoplay: true, poster: 'mobile_vert_poster_1.png'}
+    ]"
+    style=" height: 100vh;"
+    overlay="linear-gradient(45deg, rgba(38,66,71,0.577468487394958) 0%, rgba(19,37,50,0.9360119047619048) 100%)" 
+>
+    <h1 class="text-5xl text-white text-center">Hero headline goes here</h1>
+</video-background>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+
+}
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+
+</style>
